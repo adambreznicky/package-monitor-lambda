d339b31ed13b1d0bbd3eea0e548d1f111b45092e
Brez <smudgeservices@gmail.com>
2018-02-09 13:27:33 -0600

    package code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # package-monitor-lambda
 NodeJS package.json dependency monitor for AWS Lambda functions to send email notifications when packages are out of date
+
+# NodeJS
+Built with:
+* NodeJS v8.9.2
+* NPM v5.6.0
+
+# Overview
+Packaged designed to be used within AWS Lambda NodeJS6.10 Runtime functions. Identify GitHub repositories to monitor and email notification recipients as arguments. The package will identify out of date dependencies within the repo package.json file and email the list of packages with their versions. Personally, it is used within a Lambda function controlled by a fixed rate scheduled Cloudwatch rule. The package does a single inquiry with notification at the time it is being called and does not continually watch the repo, hence the scheduled Cloudwatch call of the Lambda function.
+
+Requires use of  AWS Simple Email Service (SES) for sending the emails which means you will have to configure the Lambda function with the proper role/policy permissions to fire SES and establish a verified email address for SES to utilize. Information on setting up these permissions can be found [here](http://www.wisdomofjim.com/blog/sending-an-email-from-aws-lambda-function-in-nodejs-with-aws-simple-email-service).
+
+# Arguments
+
+# Example
---
+++ index.js
@@ -0,0 +1,125 @@
+'use strict';
+var AWS = require('aws-sdk');
+var ncu = require('npm-check-updates');
+var get = require('get-file');
+var fs = require("fs");
+var R =  require('ramda');
+var input = {};
+
+function checkRepo(repo) {
+  var ses = new AWS.SES({
+     region: input.region
+  });
+
+  var owner = repo['owner'];
+  var name = repo['name'];
+  var subDir = repo['subdirectory'];
+  var githubPath = githubPath = owner + '/' + name;
+  var pkg = 'package.json';
+
+  if (subDir != '' && subDir != undefined) {
+    pkg = subDir + '/package.json';
+  }
+
+  var string = '';
+
+  get(githubPath, pkg, function(err, res) {
+    if (err) return console.error(err);
+    var file = fs.createReadStream('package.json');
+
+    res.on('data', function(buffer) {
+      var part = buffer.toString();
+      string += part;
+    })
+
+    res.on('end', function() {
+      var ownerDir = '/tmp/' + owner;
+      if (!fs.existsSync(ownerDir)){
+        fs.mkdirSync(ownerDir);
+      }
+
+      var tmpDir = '/tmp/' + githubPath;
+      console.log(tmpDir);
+      if (!fs.existsSync(tmpDir)){
+        fs.mkdirSync(tmpDir);
+      }
+      var filename = tmpDir + '/package.json';
+      var json  = JSON.parse(string);
+
+      fs.writeFile(filename, string, 'utf8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+      });
+
+      ncu.run({
+        packageFile: filename,
+        silent: false
+      }).then(function(upgrades) {
+        var numberOfUpgrades = Object.keys(upgrades).length;
+        var deps = json.dependencies;
+        var differences = 'Dependency Notice:\n\nBased on the package.json in the Master branch of the GitHub repo: ' + githubPath + '\n' + name + ' has ' + numberOfUpgrades.toString() + ' dependency updates\n\n';
+
+        function compareVersions (key) {
+          var oldVersion = deps[key];
+          var newVersion = upgrades[key];
+          if (oldVersion === undefined) {
+            oldVersion = 'NEW PACKAGE'
+          }
+          var diffStr = key + ':  ' + oldVersion + '  --->  ' + newVersion + '\n';
+          differences += diffStr;
+        }
+
+        R.keys(upgrades).forEach(compareVersions, upgrades);
+
+        console.log("EMAILING: " + githubPath);
+        var eParams = {
+          Destination: {
+              ToAddresses: input.recipients
+          },
+          Message: {
+              Body: {
+                  Text: {
+                      Data: differences
+                  }
+              },
+              Subject: {
+                  Data: 'Dependency Updates: ' + name
+              }
+          },
+          Source: input.source
+        };
+
+        var email = ses.sendEmail(eParams, function(err, data){
+            if(err) console.log(err);
+            else {
+                console.log("===EMAIL SENT===");
+            }
+        });
+
+      });
+    })
+
+  });
+}
+
+function monitor(repos, recipients, region, source) {
+  if(arguments.length < 2) {
+    throw new Error('repos and recipients arguments required in the proper format');
+  }
+
+  if (typeof repo != 'object' || Array.isArray(recipients) === false || typeof region != 'string' || typeof source != 'string') {
+    throw new Error('invalid argument type. repo must be a properly formatted object (view README). recipients must be an array of email strings. region must be an aws region string. source must be an aws ses verified email string.');
+  }
+
+  input['recipients'] = recipients;
+  input['region'] = region;
+  input['source'] = source;
+
+  R.forEach(checkRepo, repos);
+};
+
+module.exports = {
+  monitor: monitor
+};
